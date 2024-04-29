aee98b1cbb52a39280f1f6ca73e4d13382104b95
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-04-29 19:33:05 +0200

    add mysql database

---
+++ database.sql
@@ -0,0 +1,37 @@
+-- creating the database and granting all the permissions
+
+CREATE DATABASE IF NOT EXISTS LakeSite;
+CREATE USER 'arbes'@'localhost' IDENTIFIED BY 'arbes123';
+GRANT ALL PRIVILEGES ON LakeSite.* TO 'arbes'@'localhost';
+FLUSH PRIVILEGES;
+
+-- change the database to use the one we created
+USE DATABASE LakeSite; 
+
+--creating tables for users,
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- creating tables for lakes
+CREATE TABLE lakes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    location VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- create a table to maintain the reservations
+CREATE TABLE reservations (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    lake_id INT,
+    reservation_date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (lake_id) REFERENCES lakes(id)
+);
